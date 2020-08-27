d4fea54aedeba1470c9f216ab8fa00dc8f3ce96c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-08-27 19:25:26 +0300

    Added Files,Strings,Data Mining example stub

---
+++ src/main/scala/CookBook.scala
@@ -0,0 +1,22 @@
+object CookBook extends App {
+  val srcName = "13177-8.txt"
+  val dstName = "13177-8-results.txt"
+  def openSource(fName:String) = {
+    //actually get a real sequence of strings
+    Seq("abra","dabra") //just a placeholder
+  }
+
+  def processSeq(mySeq:Seq[String])= {
+    Seq("RECIPE","ingredient") //another placeholder
+  }
+
+  def saveSeq(destName:String, mySeq:Seq[String]) = {
+    println(s"Saving my Sequence to file $destName")
+    scala.util.Success
+  }
+
+  //the actual program runs here
+  val mySeq = openSource(srcName)
+  val filteredSeq = processSeq(mySeq)
+  saveSeq(dstName,filteredSeq)
+}
