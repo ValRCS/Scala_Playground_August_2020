5d0912e6f542f7eff6f5f2caaffb2573b91b3270
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-10 19:24:17 +0300

    Get lines with 7 split size

--- src/main/scala/FileInfo.scala
+++ src/main/scala/FileInfo.scala
@@ -24,6 +24,34 @@ object FileInfo extends App {
     bufferedSource.close
     count
   }
-  val lineCount = getLineCount("./src/resources/fails_par_2019_gadu.csv")
+  def getRowSplitSize(fileName:String): Int = {
+    var count = 0
+    val bufferedSource = io.Source.fromFile(fileName)
+    for (line <- bufferedSource.getLines) {
+      val splitLine = line.split(";")
+      count += splitLine.size
+    }
+    bufferedSource.close
+    count
+  }
+
+  def getLinesWithSplitSize(fileName:String, splitSize:Int): Int = {
+    var count = 0
+    val bufferedSource = io.Source.fromFile(fileName)
+    for (line <- bufferedSource.getLines) {
+      val splitLine = line.split(";")
+      //FIXME find the lines with the splitSize
+    }
+    bufferedSource.close
+    count //return how many lines are there
+  }
+
+  val filePath = "./src/resources/fails_par_2019_gadu.csv"
+  val lineCount = getLineCount(filePath)
   println(s"We got a file with $lineCount lines")
+  val rowSplitCount = getRowSplitSize(filePath)
+  println(s"We got a file with $rowSplitCount lines")
+  val linesWith7 = getLinesWithSplitSize(filePath, 7)
+  println(linesWith7)
+
 }
