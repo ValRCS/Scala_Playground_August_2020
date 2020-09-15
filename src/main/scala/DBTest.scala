119f051661bb4e994863d123f18843b4fea995ca
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-15 21:48:28 +0300

    SQL examples for unlimited column names

--- src/main/scala/DBTest.scala
+++ src/main/scala/DBTest.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable.ListBuffer
 // https://www.sqlitetutorial.net/sqlite-sample-database/
 // https://alvinalexander.com/scala/scala-jdbc-connection-mysql-sql-select-example/
 // https://www.sqlitetutorial.net/sqlite-java/sqlite-jdbc-driver/
@@ -12,10 +13,42 @@ object DBTest extends App{
   val conn = DriverManager.getConnection(url)
 
   val statement = conn.createStatement()
-  val resultSet = statement.executeQuery("SELECT * FROM ARTISTS LIMIT 10")
-  while ( resultSet.next() ) {
-    val host = resultSet.getString("ArtistId")
-    val user = resultSet.getString("name")
-    println("host, user = " + host + ", " + user)
+//  val resultSet = statement.executeQuery("SELECT * FROM ARTISTS LIMIT 10")
+
+  val sql =
+    """
+      |SELECT  *, COUNT(Total) purc_count,
+      |SUM(Total), ROUND(AVG(Total),2),
+      |ROUND(MIN(Total),2),MAX(Total)
+      |FROM v_buyers vb
+      |GROUP BY FullName
+      |ORDER BY SUM(total) DESC
+      |""".stripMargin
+//  val resultSet = statement.executeQuery("""
+//       |SELECT v."Month",
+//       |SUM(v.Total) msales, COUNT(v."Month" )
+//       |FROM v_2010 v
+//       |GROUP BY "Month"
+//       |ORDER BY msales DESC""".stripMargin)
+
+  val resultSet = statement.executeQuery(sql)
+  val meta = resultSet.getMetaData
+  println(meta.getColumnCount)
+  var colSeq = ListBuffer[String]()
+  for (i <- 1 to
+    meta.getColumnCount) {
+    println(meta.getColumnName(i))
+    colSeq += meta.getColumnName(i)
   }
+println(colSeq)
+
+  while ( resultSet.next() ) {
+
+    colSeq.foreach(col => print(col, resultSet.getString(col)))
+    println()
+//    val sales = resultSet.getString("msales")
+//    val month = resultSet.getString("Month")
+//    println("host, user = " + sales + ", " + month)
+  }
+
 }
