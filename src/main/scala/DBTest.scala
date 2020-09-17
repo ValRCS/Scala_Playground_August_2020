15aedb0b891b634c22130639ad4122e9f2e50d46
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-17 21:53:36 +0300

    Saving SQL query results into Seq of Sequences

--- src/main/scala/DBTest.scala
+++ src/main/scala/DBTest.scala
@@ -15,14 +15,23 @@ object DBTest extends App{
   val statement = conn.createStatement()
 //  val resultSet = statement.executeQuery("SELECT * FROM ARTISTS LIMIT 10")
 
+//  val sql =
+//    """
+//      |SELECT  *, COUNT(Total) purc_count,
+//      |SUM(Total), ROUND(AVG(Total),2),
+//      |ROUND(MIN(Total),2),MAX(Total)
+//      |FROM v_buyers vb
+//      |GROUP BY FullName
+//      |ORDER BY SUM(total) DESC
+//      |""".stripMargin
   val sql =
     """
-      |SELECT  *, COUNT(Total) purc_count,
-      |SUM(Total), ROUND(AVG(Total),2),
-      |ROUND(MIN(Total),2),MAX(Total)
-      |FROM v_buyers vb
-      |GROUP BY FullName
-      |ORDER BY SUM(total) DESC
+      |SELECT p.Name, COUNT(pt.TrackId) nr_of_tracks FROM playlists p
+      |JOIN playlist_track pt
+      |ON pt.PlaylistId = p.PlaylistId
+      |GROUP BY p.Name
+      |ORDER BY nr_of_tracks DESC
+      |LIMIT 10
       |""".stripMargin
 //  val resultSet = statement.executeQuery("""
 //       |SELECT v."Month",
@@ -41,14 +50,21 @@ object DBTest extends App{
     colSeq += meta.getColumnName(i)
   }
 println(colSeq)
-
+//resultSet.next() means it will return some row or false / falsy
+  val resultsBuffer = scala.collection.mutable.ListBuffer.empty[Seq[(String,String)]]
   while ( resultSet.next() ) {
 
-    colSeq.foreach(col => print(col, resultSet.getString(col)))
-    println()
-//    val sales = resultSet.getString("msales")
-//    val month = resultSet.getString("Month")
-//    println("host, user = " + sales + ", " + month)
+//    colSeq.foreach(col => print(col, resultSet.getString(col)))
+    val row = colSeq.map(col => (col, resultSet.getString(col))) //TODO think of other structures to hold data
+    resultsBuffer.append(row.toSeq)
+    println(row.size)
   }
+  val results = resultsBuffer.toSeq
+  println(results.size)
+  results.foreach(line => println(line.getClass, line.size))
+  println(results.getClass)
+  val classical = results.filter(line => line(0)._2.startsWith("Classical"))
+  classical.foreach(println)
 
+//  val newResults = results.map(line => line.toSeq)
 }
