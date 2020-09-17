322b8409b63673b95808a437714f834a25197efe
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-17 19:50:43 +0300

    First filtering stage for Cookbook

--- src/main/scala/CookBook.scala
+++ src/main/scala/CookBook.scala
@@ -3,14 +3,14 @@ import java.io.FileWriter
 object CookBook extends App {
 //  val srcName = "13177-8.txt"
 //  val dstName = "13177-8-results.txt"
-  val srcName = "c:/poems/frost.txt"
-  val dstName = "c:/poems/frost_cut.txt"
+  val srcName = "c:/temp/13177-8.txt"
+  val dstName = "c:/temp/13177-8-cleaned.txt"
   def openSource(fName:String) = {
     //actually get a real sequence of strings
     val filePointer = scala.io.Source.fromFile(srcName)
     val myLines = filePointer.getLines.toSeq
 
-    filePointer.close()
+//    filePointer.close()
     myLines
   }
 
@@ -18,19 +18,15 @@ object CookBook extends App {
     //TODO actually do the assignment
     mySeq.slice(0,8) //just some lines
     //iterative solution
-    var newSeq: Seq[String] = Seq()
-    for (i <- 0 to 8 by 2) {
-      newSeq = newSeq :+ mySeq(i) //so we keep making new sequence using old one as base
-    }
-    newSeq
-    //functional solution
-    val funSeq = mySeq.zipWithIndex.filter(t => t._2 % 2 == 0 && t._2 < 12).map(t => t._1) //all even lines under 8
-    funSeq
+    //TODO filter by multiple lines meaning CAPITAL then possible Illustration then 4 spaces
+    val ingredients = mySeq.filter(line => line.startsWith("    ")) //TODO add titles possibly with regex
+    //TODO more filtering for Illustrations and of course false titles meaning titles which do not have ingredients following
+    ingredients
   }
 
   def saveSeq(destName:String, mySeq:Seq[String]) = {
     println(s"Saving my Sequence to file $destName")
-    mySeq.foreach(println) //we are good up to here
+//    mySeq.foreach(println) //we are good up to here
     val fw = new FileWriter(destName)
     mySeq.map(_ + "\n").foreach(fw.write) // adding new line to each line before writing
     fw.close()
