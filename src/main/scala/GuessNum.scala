db206f2da19ea403f4c1f1a73c340cb9f7132743
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-21 19:43:36 +0300

    Play until win guess the number with congrats message

--- src/main/scala/GuessNum.scala
+++ src/main/scala/GuessNum.scala
@@ -1,22 +1,29 @@
 import scala.io.StdIn.{readInt, readLine}
 import scala.util.Random
+//import scala.util.control.Breaks.break
 
 object GuessNum extends App {
   println("Guess the number game")
   val nmin = 1
   val nmax = 100
+  val maxTries = 10
   val trueNum = Random.between(nmin, nmax)
   val userName = readLine("What is your name?")
   println(s"What is your guess $userName? Enter number between $nmin and $nmax")
   var userGuess = readInt()
-  while (userGuess != trueNum) {
+  var gCount = 1
+  while (userGuess != trueNum && gCount <= maxTries) {
     if (userGuess > trueNum)
-        println(s"$userGuess TOO BIG! Try again ")
+        println(s"Guess No. $gCount: $userGuess TOO BIG! Try again ")
     else {
-      println(s"$userGuess SMALL! Try again ")
+        println(s"Guess No. $gCount: $userGuess SMALL! Try again ")
     }
+    gCount += 1
     userGuess = readInt()
   }
-  print(s"Nice you won! the number indeed is $trueNum")
+  if (userGuess == trueNum )
+    println(s"Nice you won in $gCount tries! the number indeed is $trueNum")
+  else
+    println(s"Sorry you lost in $gCount tries, your guess was $userGuess while my number is $trueNum")
   //TODO get some exit code perhaps
 }
