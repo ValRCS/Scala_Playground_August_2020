9d886fd21784c6871f11dd831648a9fb7c38c1c7
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-24 21:44:43 +0300

    Added Batch updates

---
+++ src/main/scala/Contact.scala
@@ -0,0 +1,5 @@
+final case class Contact(contact_id: Int,
+      first_name: String,
+      last_name: String,
+      email: String,
+      phone: String)
--- src/main/scala/CreateDB.scala
+++ src/main/scala/CreateDB.scala
@@ -1,4 +1,4 @@
-import java.sql.DriverManager
+import java.sql.{DriverManager, PreparedStatement}
 
 
 object CreateDB extends App {
@@ -13,7 +13,7 @@ object CreateDB extends App {
 //  for ((k,v) <- properties) println(s"key: $k, value: $v")
     val sqlite_home = environmentVars.get("SQLITE_HOME").replace("\\", "/")
 
-  val dbname = "really_newone.db"
+  val dbname = "phone.db"
   println(s"Creating DB $dbname")
   val url = s"jdbc:sqlite:$sqlite_home/db/$dbname"
 
@@ -26,14 +26,50 @@ object CreateDB extends App {
       |	contact_id INTEGER PRIMARY KEY,
       |	first_name TEXT NOT NULL,
       |	last_name TEXT NOT NULL,
-      |	email TEXT NOT NULL UNIQUE,
-      |	phone TEXT NOT NULL UNIQUE
+      |	email TEXT NOT NULL ,
+      |	phone TEXT NOT NULL
       |);
       |""".stripMargin
 
   val statement = conn.createStatement()
   val resultSet = statement.execute(sql)
 
+  val person1 = Contact(1, "John", "Smith", "john.smith@gmail.com", "888223408")
+  val person2 = Contact(2, "Mary", "Simpson", "mary.simpson@gmail.com", "885993500")
+  val person3= Contact(3, "Homer", "Simpson", "homer.simpson@gmail.com", "885993500")
+  val persons = Seq(person1, person2, person3)
+  persons.foreach(println)
+
+  val insertSql =
+    """
+      |INSERT INTO contacts (
+      |first_name, last_name, email, phone)
+      |VALUES(?,?,?,?)
+      |""".stripMargin
+
+  val preparedStatement = conn.prepareStatement(insertSql)
+  for (p <- persons) {
+//    preparedStatement.setInt(1, p.contact_id)
+    preparedStatement.setString(1, p.first_name)
+    preparedStatement.setString(2, p.last_name)
+    preparedStatement.setString(3, p.email)
+    preparedStatement.setString(4, p.phone)
+    preparedStatement.addBatch
+  }
+
+  // so this below is same as above 5 lines
+  persons.foreach(setPerson(preparedStatement, _))
+  preparedStatement.executeBatch()
+
+  preparedStatement.close()
+
+  def setPerson(preparedStatement: PreparedStatement, person:Contact) = {
+      preparedStatement.setString(1, person.first_name)
+      preparedStatement.setString(2, person.last_name)
+      preparedStatement.setString(3, person.email)
+      preparedStatement.setString(4, person.phone)
+      preparedStatement.addBatch
+  }
 
 
 }
